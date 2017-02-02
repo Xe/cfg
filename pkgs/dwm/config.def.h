6e07fda12b55e407ee29bb8fb6021efe5c6161c2
Christine Dodrill <me@christine.website>
2017-02-01 21:34:01 -0800

    gruvbox colors and spawn cmd in same working directory of window

--- pkgs/dwm/config.def.h
+++ pkgs/dwm/config.def.h
@@ -13,17 +13,20 @@ enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always
 static const int showtab            = showtab_auto; /* Default tab bar show mode */
 static const int toptab             = False;    /* False means bottom tab bar */
 
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+#include "colors.h"
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { light1, dark0, gray_245 },
+	[SchemeSel] =  { col_gray4, dark2,  gray_244  },
 };
 
 /* tagging */
