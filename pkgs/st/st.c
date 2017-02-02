740bb01459de7663d12e504d75bfa59c866f4f4b
Christine Dodrill <me@christine.website>
2017-02-01 20:42:00 -0800

    selection sets CLIPBOARD and PRIMARY

--- pkgs/st/st.c
+++ pkgs/st/st.c
@@ -1287,6 +1287,8 @@ xsetsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear(0);
+
+	clipcopy(NULL);
 }
 
 void
